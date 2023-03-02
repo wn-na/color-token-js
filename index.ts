2468625bb5b1f2c37ce8ff71d26da2ec68386a66
Lethe <37437842+0xlethe@users.noreply.github.com>
2023-03-02 17:22:20 +0900

    feat: v1.0.2

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 export { generateColorPalette } from "./utils/palette";
+export * as color from "./utils/color";
+export * as adjustment from "./utils/adjustment";
+export * as type from "./utils/type";
